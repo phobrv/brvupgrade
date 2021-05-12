e2c43890ffa4de36c4bafece72ba018a11c9a8b0
Nguyen Trong Pho <nguyentrongpho@gmail.com>
2021-05-12 17:02:25 +0700

    update

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 Route::middleware(['web', 'auth', 'auth:sanctum', 'lang', 'verified'])->namespace('Phobrv\BrvUpgrade\Controllers')->group(function () {
 	Route::middleware(['can:superuser'])->prefix('admin')->group(function () {
-		Route::get('/upgrade', 'UpgradeControllerr@index')->name('upgrade.index');
+		Route::get('/upgrade', 'UpgradeController@index')->name('upgrade.index');
 		Route::get('/upgrade/replace', 'UpgradeController@replace')->name('upgrade.replace');
 		Route::post('/upgrade/run', 'UpgradeController@run')->name('upgrade.run');
 	});
